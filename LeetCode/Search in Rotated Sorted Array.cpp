46bfa9d1d19c3bf9aeff38f37dc74dc3609f4cd4
changmu <2276479303@qq.com>
2015-02-14 17:05:53 +0800

    first commit

---
+++ LeetCode/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,30 @@
+/************************************************************* 
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ *************************************************************/ 
+class Solution {
+public:
+    int search(int A[], int n, int target) {
+        int left = 0, right = n - 1;
+		int index = -1;
+
+		while (left <= right) {
+			int mid = left + right >> 1;
+			if (target == A[mid]) return mid;
+
+			if (A[mid] >= A[left]) {
+				if (target > A[mid]) left = mid + 1;
+				else if (target >= A[left]) right = mid - 1;
+				else left = mid + 1;
+			} else {
+				if (target < A[mid]) right = mid - 1;
+				else if (target <= A[right]) left = mid + 1;
+				else right = mid - 1;
+			}
+		}
+
+		return index;
+    }
+};
