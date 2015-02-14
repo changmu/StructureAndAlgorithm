c73f86f29468c1395eccc1a584727c0055be8e8d
changmu <2276479303@qq.com>
2015-02-14 17:22:00 +0800

    update

--- LeetCode/Search in Rotated Sorted Array.cpp
+++ LeetCode/Search in Rotated Sorted Array.cpp
@@ -8,23 +8,20 @@ class Solution {
 public:
     int search(int A[], int n, int target) {
         int left = 0, right = n - 1;
-        int index = -1;
 
         while (left <= right) {
             int mid = left + right >> 1;
             if (target == A[mid]) return mid;
 
             if (A[mid] >= A[left]) {
-                if (target > A[mid]) left = mid + 1;
-                else if (target >= A[left]) right = mid - 1;
+                if (target < A[mid] && target >= A[left]) right = mid - 1;
                 else left = mid + 1;
             } else {
-                if (target < A[mid]) right = mid - 1;
-                else if (target <= A[right]) left = mid + 1;
+                if (target > A[mid] && target <= A[right]) left = mid + 1;
                 else right = mid - 1;
             }
         }
 
-        return index;
+        return -1;
     }
 };
