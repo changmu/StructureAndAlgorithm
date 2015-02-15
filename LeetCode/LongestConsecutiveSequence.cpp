ab5aad54b593f13101c371cc0dbf49ae768d2b48
changmu <2276479303@qq.com>
2015-02-15 15:51:31 +0800

    add a file

--- LeetCode/LongestConsecutiveSequence.cpp
+++ LeetCode/LongestConsecutiveSequence.cpp
@@ -14,6 +14,33 @@ using namespace std;
 
 class Solution {
 public:
+    int longestConsecutive(vector<int>& num) {
+        if (num.empty()) return 0;
+        unordered_map<int, int> map;
+        int longest = 1;
+
+        for (auto i : num) {
+            if (map.find(i) != map.end()) continue;
+
+            map[i] = 1;
+            if (map.find(i + 1) != map.end())
+                longest = max(longest, Merge(map, i, i + 1));
+            if (map.find(i - 1) != map.end())
+                longest = max(longest, Merge(map, i - 1, i));
+        }
+
+        return longest;
+    }
+
+private:
+    int Merge(unordered_map<int, int>& map, int left, int right) {
+        int upper = left + map[right];
+        int lower = right - map[left];
+        int length = upper - lower + 1;
+
+        return map[lower] = map[upper] = length;
+    }
+/*    
     int longestConsecutive(vector<int> &num) {
         unordered_map<int, bool> used;        
         for (auto i : num) used[i] = false;
@@ -39,14 +66,14 @@ public:
         }
 
         return longest;
-    }
+    }*/
 };
 
 int main()
 {
     Solution sol;
-    int a[] = {100, 4, 200, 1, 3, 2};
-    vector<int> num(a, a + 6);
+    int a[] = {1,3,5,2,4};
+    vector<int> num(a, a + 5);
     printf("%d\n", sol.longestConsecutive(num));
     return 0;
 }
