e6b4685baa655edec36161c3a90c67fc59eaa98f
changmu <2276479303@qq.com>
2015-03-14 17:40:54 +0800

    add a method

--- LeetCode/ReverseLinkedListII.c
+++ LeetCode/ReverseLinkedListII.c
@@ -5,6 +5,7 @@
  *     struct ListNode *next;
  * };
  */
+// Method 1: Time: O(n), Space O(n)
 struct ListNode *reverseBetween(struct ListNode *head, int m, int n) {
 	struct ListNode pHeadVirtual;
 	pHeadVirtual.next = head;
@@ -35,4 +36,36 @@ struct ListNode *reverseBetween(struct ListNode *head, int m, int n) {
 	free(stack);
 
 	return pHeadVirtual.next;
+}
+
+// Method 2: Time O(n), Space O(1)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *reverseBetween(struct ListNode *head, int m, int n) {
+	struct ListNode dummy;
+	dummy.next = head;
+	struct ListNode *pPre = NULL, *pAfter = NULL;
+	struct ListNode *pNode = &dummy;
+	int count = 0;
+
+	for ( ; count < m - 1; ++count)
+		pNode = pNode->next;
+	pPre = pNode;
+	pAfter = pNode->next;
+	pNode = pAfter->next;
+	
+	for ( count += 2; count <= n; ++count) {
+		/* Drawing is good for understanding */
+		pAfter->next = pNode->next;
+		pNode->next = pPre->next;
+		pPre->next = pNode;
+		pNode = pAfter->next;
+	}
+
+	return dummy.next;
 }
